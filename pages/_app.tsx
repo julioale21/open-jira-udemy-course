2e1908b5e355afa89cba63316797d39f26c3bc9f
Julio Romero <julio.romero@go1.com>
2022-04-03 21:10:17 -0300

    added basic theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,20 @@
+import * as React from 'react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { ThemeProvider } from '@mui/system';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const basicTheme = createTheme({
+  palette: {
+    mode: 'dark'
+  } 
+});
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={}>
+    <ThemeProvider theme={basicTheme}>
+      <CssBaseline />
       <Component {...pageProps} />
     </ThemeProvider>
   );
