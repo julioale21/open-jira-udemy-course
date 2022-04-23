299e07aa9e452f6a3db21d4ffc92d309ad2dacf5
Julio Romero <julio.romero@go1.com>
2022-04-23 14:15:46 -0300

    added board columns

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,29 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import { Card, CardContent, CardHeader, Grid, Typography } from "@mui/material";
 import { Layout } from "../components/layouts";
 
 export function HomePage() {
   return (
-    <Layout>
-      <Typography color="secondary" variant="h1">
-        Hola Mundo
-      </Typography>
+    <Layout title="Home - Open Jira">
+      <Grid container spacing={2}>
+        <Grid item sm={4} xs={12}>
+          <Card sx={{ height: "calc(100vh - 100px)" }}>
+            <CardHeader title="To Do" />
+            <CardContent>{/* add new task */}</CardContent>
+            <CardContent>{/* listing tasks */}</CardContent>
+          </Card>
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <Card sx={{ height: "calc(100vh - 100px)" }}>
+            <CardHeader title="In Progress" />
+          </Card>
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <Card sx={{ height: "calc(100vh - 100px)" }}>
+            <CardHeader title="Done" />
+          </Card>
+        </Grid>
+      </Grid>
     </Layout>
   );
 }
